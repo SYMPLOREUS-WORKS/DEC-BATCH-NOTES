6d31281f39025f5a5c0bbf729165c567231eb4c8
nsgbharath <reddysbharath45@gmail.com>
2024-12-04 08:11:30 +0530

    java day 1

---
+++ FirstProgram.java
@@ -0,0 +1,14 @@
+
+
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		if(args.length>0)
+		System.out.println("Hi, "+args[0]+"\nWelocome to Java Tutorials");
+                else
+		System.out.println("Hi, Welocome to Java Tutorials");
+
+	}
+
+}
